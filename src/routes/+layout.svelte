95c77fb77026f15095a7be07c79c8e3a6b299688
Edmundo Ortega <ed.ortega@gmail.com>
2022-12-11 21:38:38 -0800

    added routes and updated layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,33 @@
 <script>
 	
-	import '@skeletonlabs/skeleton/themes/theme-rocket.css';
+	//import '@skeletonlabs/skeleton/themes/theme-rocket.css';
+	import '../theme.postcss'
 	import '@skeletonlabs/skeleton/styles/all.css';
 	import '../app.postcss';
+
+	import {AppShell, AppBar} from '@skeletonlabs/skeleton'
 </script>
 
-<slot />
+<AppShell>
+    <!-- Header -->
+	<svelte:fragment slot="header">
+		<!-- Insert the App Bar: -->
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<h1>Skeleton</h1>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<a class="btn btn-sm" href="https://github.com/" target="_blank" rel="noreferrer">GitHub</a>
+			</svelte:fragment>
+		</AppBar>
+		<!-- --- -->
+	</svelte:fragment>    <!-- Sidebar -->
+    <!-- Page Content Slot -->
+    <slot />
+    <!-- Footer Slots -->
+	<svelte:fragment slot="pageFooter">Page Footer</svelte:fragment>
+	<svelte:fragment slot="footer">Footer</svelte:fragment>
+
+</AppShell>
+
+
